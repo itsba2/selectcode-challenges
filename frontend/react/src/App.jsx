3809ef56f54fc9c434713fa45bdd17f9beb1e305
itsba2 <s.batuhanbilmez@gmail.com>
2023-08-13 23:42:11 +0200

    initial chess board

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import Board from "./components/Board/Board";
 
 function App() {
-  return <div>Board</div>;
+  return <Board />;
 }
 
 export default App;
---
+++ frontend/react/src/components/Board/Board.jsx
@@ -0,0 +1,23 @@
+import "./Board.css";
+import Square from "./Square";
+import { initialBoard } from "./initialBoard";
+
+const Board = () => {
+  let board = initialBoard;
+
+  return (
+    <div className="board">
+      {board.map((row, rowIndex) =>
+        row.map((square, squareIndex) => (
+          <Square
+            key={`row${rowIndex}sqr${squareIndex}`}
+            color={square?.color}
+            piece={{ type: square?.piece?.type, color: square?.piece?.color }}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Board;
