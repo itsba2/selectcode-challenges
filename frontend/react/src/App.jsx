a7f2a704dee7cab9d84265a2131272e16605a41b
itsba2 <s.batuhanbilmez@gmail.com>
2023-08-13 23:28:46 +0200

    Revert "chessboard layout"
    This reverts commit 9e061cbe2216801ef56658b5dce54994df272caa.

--- frontend/react/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import Board from "./components/Board/Board";
-import { initialBoard } from "./data/initialBoard";
-
-function App() {
-  return <Board board={initialBoard} />;
-}
-
-export default App;
